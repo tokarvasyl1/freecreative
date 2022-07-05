c7cc26f18c0d17e74de30fe57f2c5748db12f141
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-07-05 15:15:10 +0300

    test commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel( {
+        loop:true,
+        nav:true,
+        dots:true,
+        margin:0,
+        navText: [ '', ' ' ],
+        mouseDrag:false,
+        touchDrag:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        smartSpeed:3500,
+
+        responsive:{
+            0:{
+                items:1
+
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
